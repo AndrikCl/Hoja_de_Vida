2a5854bce836c393c9f2414d55ba4fbc1a14f584
AndrikCl <andrikannye3@gmail.com>
2021-08-11 14:22:40 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF=8">
+        <META http-equiv="X-UA-Compatible" content="=IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">"
+        <title>Andrik Clavijo M. </title>
+    </head>
+    <body>
+        <h1 class="text-center ">ANDRIK CLAVIJO M.</h1>
+        <img src="https://www.pngkit.com/png/full/202-2022283_usuario-icono-perfil-de-usuario.png" class="rounded mx-auto d-block" alt="CENTER">
+        <br>    <div class="mb-3">
+            <p class="text-center fs-2 fw-bold">Datos personales:</p>
+            <p class="text-center fs-4">Telefono: 313589589 </p>
+            <p class="text-center fs-4">Correo: aclavijo@hotmail.com</p>
+            <p class="text-center fs-4">Profesion: Ingeniero Industrial</p>
+            <p class="text-center fs-2 fw-bold">Descripcion:</p>
+            <p class="text-center fs-4">Me considero una persona muy eficiente, que siempre busca la optimizacion de las tareas asignadas, trabajando en equipo para solucionar las diferentes adversidades que surgen diariamente.</p>
+          </div>
+          <br>
+          <div class="shadow-lg p-3 mb-5 bg-body rounded text-center fs-1 fw-bold text-muted">Estudios</div>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Universidad Nacional</th>
+                    <th scope="col"> </th>
+                    <th scope="col">SENA</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Ingeniero Industrial</td>
+                    <td></td>
+                    <td>Tecnologo en Gestion de Mercados</td>
+                  </tr>
+                  <tr>
+                    <td>2016</td>
+                    <td></td>
+                    <td>2020</td>
+                  </tr>
+                </tbody>
+              </table></div>
+          <div class="shadow-lg p-3 mb-5 bg-body rounded text-center fs-1 fw-bold text-muted">Experiencia Profesional</div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Tornillos 777</th>
+                <th scope="col">Grupo Aval</th>
+                <th scope="col">Hipermercados Alkosto</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Cajero</td>
+                <td>Archivista</td>
+                <td>Administrador</td>
+              </tr>
+              <tr>
+                <td>2012</td>
+                <td>2015</td>
+                <td>2019</td>
+              </tr>
+            </tbody>
+          </table></div>
+          <div class="shadow-lg p-3 mb-5 bg-body rounded text-center fs-1 fw-bold text-muted">Programas e Idiomas</div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Programa</th>
+                <th scope="col">25%</th>
+                <th scope="col">50%</th>
+                <th scope="col">75%</th>
+                <th scope="col">100%</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="col">Excel</th>
+                <td></td>
+                <td></td>
+                <td>x</td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="col">Word</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>x</td>
+              </tr>
+              <tr>
+                <th scope="col">Power Point</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>x</td>
+              </tr>
+              <tr>
+                <th scope="col">Gimp</th>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="col">Ingles</th>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="col">Frances</th>
+                <td>x</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table></div>
+          <div class="shadow-lg p-3 mb-5 bg-body rounded text-center fs-1 fw-bold text-muted">Hobbies</div>
+          <table class="table">
+            <thead>
+              <tr>
+                <td><img src="https://cdn0.iconfinder.com/data/icons/sport-and-hobby-elasto-font-next-2020/16/17_bicycle-512.png" style="width: 250px" alt=""></td>
+                <td><img src="https://toppng.com/uploads/preview/royalty-free-clip-art-silueta-de-libro-abierto-11563010067o1ymdd96kg.png" style="width: 225px" alt=""></td>
+                <td><img src="https://e7.pngegg.com/pngimages/358/458/png-clipart-hiking-backcountry-com-computer-icons-trail-others-text-monochrome.png" style="width: 380px" alt=""></td>
+                <td><img src="https://image.flaticon.com/icons/png/512/67/67745.png" style="width: 250px" alt=""></td>
+              </tr>
+            </thead>
+            </tbody>
+          </table></div>
+    </body>
+
+</html>
